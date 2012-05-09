dcc0249b0fdab873226f3e694a41f5e139510b62
John Stovin <john.d.stovin@gmail.com>
2012-05-09 22:32:58 +0100

    Initial unit test

---
+++ MetroNetAudioTest/XAudio2Test.cs
@@ -0,0 +1,17 @@
+﻿namespace MetroNetAudioTest
+{
+    using MetroNetAudio;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class XAudio2Test
+    {
+        [TestMethod]
+        public void CanCreateObject()
+        {
+            XAudio2 obj = XAudio2.Create();
+
+            Assert.IsNotNull(obj);
+        }
+    }
+}
